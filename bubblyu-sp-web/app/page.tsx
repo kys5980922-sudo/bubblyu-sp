7acca53d65ac3315b2dbec99f600a6ca9cbfcde2
kys5980922-sudo <kys5980922@gmail.com>
2026-05-09 02:37:31 +0900

    Update page.tsx

--- bubblyu-sp-web/app/page.tsx
+++ bubblyu-sp-web/app/page.tsx
@@ -148,11 +148,11 @@ export default function BubblyuSP() {
 
           <div className="grid md:grid-cols-3 gap-6">
 [
-  "/promotion/price-guide.webp.png",
-  "/promotion/trade-guide.webp.png",
-  "/promotion/clan-partner.webp.png",
-  "/promotion/bubblyu-promotion.webp.png",
-  "/promotion/report-guide.webp.png",
+  "/promotion/price-guide.png",
+  "/promotion/trade-guide.png",
+  "/promotion/clan-partner.png",
+  "/promotion/bubblyu-promotion.png",
+  "/promotion/report-guide.png",
 ]
 ].map((src, index) => (
   <div
